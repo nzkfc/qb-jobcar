3b87a0d7f8543e780754f031442c2ae0fecfd329
nzkfc <nzryancas@gmail.com>
2022-09-22 18:09:34 +1200

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,13 @@
+local Translations = {
+    text = {
+        job_vehicles = "Job Vehicles",
+        close_menu = "⬅ Close Menu",
+        store_vehicle = "~g~E~w~ - Store Vehicle",
+        vehicles = "~g~E~w~ - Job Vehicles",
+    }
+}
+
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
